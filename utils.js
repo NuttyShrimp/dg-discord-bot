9128b8c3bc131966de4edbc1519657bfa65d6701
NuttyShrimp <jan.lecoutere@hotmail.be>
2021-08-07 19:42:01 +0200

    Added embeds

---
+++ utils.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+
+const parseAttachments = (message, _msg) => {
+	const msg = _msg || message.content
+	if (message.attachments.size > 0) {
+		for (const attachment of message.attachments.entries()) {
+			const img = attachment[1];
+			if (['.jpg', '.png', '.gif', '.jpeg', '.bmp'].includes(path.extname(img.name.toLowerCase()))) {
+				msg += '\n' + img.url;
+			}
+		}
+	}
+	return msg;
+}
+
+const createMsgEmbed = (title, msg, author, authorimg) => {
+	let embed = new Discord.MessageEmbed()
+	.setAuthor(author, authorimg)
+	.setColor('#E95578')
+	.setTitle(title)
+	.setDescription(msg)
+	.setTimestamp(new Date());
+	return embed;
+}
+
+module.exports = {
+	parseAttachments,
+	createMsgEmbed
+}
