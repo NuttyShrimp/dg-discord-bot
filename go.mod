f694722005b8773110705eb17683b436b928ac99
NuttyShrimp <jan.lecoutere@hotmail.be>
2023-02-06 22:50:41 +0100

    feat: add sentry hook to logrus (hopefully)

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module degrens/bot
 go 1.19
 
 require (
+	github.com/aidenwallis/go-utils v0.0.15
 	github.com/bwmarrin/discordgo v0.26.1
+	github.com/getsentry/sentry-go v0.17.0
 	github.com/joho/godotenv v1.4.0
 	github.com/onrik/gorm-logrus v0.5.0
 	github.com/sirupsen/logrus v1.9.0
@@ -12,11 +14,11 @@ require (
 )
 
 require (
-	github.com/aidenwallis/go-utils v0.0.15 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
+	golang.org/x/crypto v0.0.0-20220926161630-eccd6366d1be // indirect
 	golang.org/x/sys v0.4.0 // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
--- internal/common.go
+++ internal/common.go
@@ -6,9 +6,12 @@ import (
 	"degrens/bot/internal/common"
 	"degrens/bot/internal/db"
 	"degrens/bot/internal/run"
+	"degrens/bot/internal/sentryhook"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/getsentry/sentry-go"
+	"github.com/sirupsen/logrus"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -19,6 +22,18 @@ func Init() {
 	}
 
 	log.Info("Starting DG discord bot")
+
+	sentry.Init(sentry.ClientOptions{
+		// Either set your DSN here or set the SENTRY_DSN environment variable.
+		Dsn: "https://e956f2798bfc4b7d89f14f6e0880eb2e@sentry.nuttyshrimp.me/13",
+		// Enable printing of SDK debug messages.
+		// Useful when getting started or trying to figure something out.
+		Debug: false,
+	})
+
+	sentryhook := sentryhook.Hook{}
+	logrus.AddHook(sentryhook)
+
 	// TODO: add sentry hook
 	if err = setupGlobalDGoSession(); err != nil {
 		log.WithError(err).Fatal("Failed to create discordgo session")
