e6109e8c9ff1a206962bf06931ab9b590631c612
NuttyShrimp <jan.lecoutere@hotmail.be>
2023-02-22 10:14:48 +0100

    fix(cfx): use right server convar to get connected count

--- internal/plugins/cfx/models.go
+++ internal/plugins/cfx/models.go
@@ -2,7 +2,7 @@ package cfx
 
 type ServerInfo struct {
 	Vars struct {
-		Connected int `json:"sv_queueConnectedCount"`
+		Connected int `json:"sv_connectedCount"`
 		Queue     int `json:"sv_queueCount"`
 	} `json:"vars"`
 }
